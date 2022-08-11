56b516af6b6d8cb3c3ed56a9dea14bf8128c0f79
applex90 <alex20bachmann@gmx.de>
2022-08-11 12:47:12 +0200

    Authguard deleted

--- slackclone/src/app/app-routing.module.ts
+++ slackclone/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
 import { ChatareaComponent } from './chatarea/chatarea.component';
 import { LoginComponent } from './login/login.component';
 
@@ -8,7 +7,7 @@ const routes: Routes = [
   {
     path: '',
     component: ChatareaComponent,
-    canActivate: [AuthGuard]
+
   },
   {
     path: 'login',
