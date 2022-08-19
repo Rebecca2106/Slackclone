6b7f33554ec3624d9e127496ab22edf331eba421
applex90 <alex20bachmann@gmx.de>
2022-08-19 14:27:53 +0200

    deleted cke deps

--- slackclone/src/app/chat-main/chat-main.component.ts
+++ slackclone/src/app/chat-main/chat-main.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UiChangeService } from '../services/ui-change.service';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-chat-main',
@@ -9,7 +8,6 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class ChatMainComponent implements OnInit {
   
-  public Editor = ClassicEditor;
     constructor(public uiService: UiChangeService) { 
   }
 
