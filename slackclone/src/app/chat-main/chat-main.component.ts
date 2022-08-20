e8576e7e87596a805be1b34a0f8951890507bd1b
KevinWissmer <kevin.wissmer@mail.de>
2022-08-20 10:36:16 +0200

    chat main changed

--- slackclone/src/app/chat-main/chat-main.component.ts
+++ slackclone/src/app/chat-main/chat-main.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UiChangeService } from '../services/ui-change.service';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-chat-main',
@@ -8,8 +7,7 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./chat-main.component.scss']
 })
 export class ChatMainComponent implements OnInit {
-  
-  public Editor = ClassicEditor;
+
     constructor(public uiService: UiChangeService) { 
   }
 
