0c27cdb344fc761e46e77893e2f7568052ed561d
KevinWissmer <kevin.wissmer@mail.de>
2022-08-27 13:29:29 +0200

    thread changed

--- slackclone/src/app/chat-thread/chat-thread.component.ts
+++ slackclone/src/app/chat-thread/chat-thread.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseChannelChatThreadService } from 'src/app/services/firebase-channel-chat-thread.service';
 import { UiChangeService } from '../services/ui-change.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { FireauthService } from '../services/fireauth.service';
+import tinymce from 'node_modules/tinymce';
+import firebase from 'firebase/compat/app';
+import { Message } from 'src/models/message.class';
+import { FirebaseChannelChatThreadService } from 'src/app/services/firebase-channel-chat-thread.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FirebaseMainService } from '../services/firebase-main.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FirebaseChatService } from 'src/app/services/firebase-chat.service';
+import { FirebaseChannelService } from 'src/app/services/firebase-channel.service';
+import { finalize, timestamp } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-thread',
@@ -9,10 +20,89 @@ import { UiChangeService } from '../services/ui-change.service';
 })
 export class ChatThreadComponent implements OnInit {
 
-  constructor(public fcctService: FirebaseChannelChatThreadService, public uiService: UiChangeService) { 
+  noteText: string;
+  message: Message;
+  messageImages = [];
+  isUploading: boolean = false;
+  newContent = "";
+  currentUploadImage: string;
+  uploadData = "";
+  filepathID: string;
+
+
+  constructor( public channelService: FirebaseChannelService, public chatService: FirebaseChatService,public fsMain: FirebaseMainService ,public sanitizer: DomSanitizer, public fcctService: FirebaseChannelChatThreadService, public uiService: UiChangeService, private storage: AngularFireStorage, public fs: FireauthService, private firestore: AngularFirestore) { 
   }
 
   ngOnInit(): void {
   }
 
+  logChange() {
+    console.log(this.noteText);
+  }
+
+  
+  createUniquefilepathID(event) {
+    this.filepathID = `${event.timeStamp}${event.target.files[0].size}`;
+    this.filepathID = this.filepathID.replaceAll('.', '');
+  }
+
+  upload(event: any) {
+    this.isUploading = true;
+    this.createUniquefilepathID(event);
+    const file = event.target.files[0];
+    const filePath = `message_images/${this.filepathID}_msgimage`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+      finalize(() => { // Execute when the observable completes
+        fileRef.getDownloadURL().subscribe(downloadURL => {
+          this.messageImages.push(downloadURL);
+          this.currentUploadImage = downloadURL;
+          console.log('messageImages', this.messageImages);
+          this.isUploading = false;
+        });
+      })
+    ).subscribe();
+  };
+
+  clearInput() {
+    this.uploadData = '';
+    this.noteText = '';
+    this.newContent = '';
+    this.messageImages = [];
+    this.setContentToEditor(''); //clear input
+    tinymce.activeEditor.focus();
+  }
+
+  setContentToEditor(data: string) {
+    tinymce.activeEditor.setContent(data, { format: 'html' });
+  }
+
+  addMessage() {
+    console.log('dd');
+    
+    if (this.fcctService.midContent.type == 'chat' || this.fcctService.midContent.type == 'channel') {
+
+      let message = new Message();
+      message.timestamp = firebase.firestore.Timestamp.now();
+      message.creator = this.fs.user.uid;
+      message.message = this.noteText;
+      message.images = this.messageImages;
+      this.fcctService.midContent.messages.push(message.toJSON());
+
+      if (this.fcctService.midContent.type == 'channel') {
+        this.channelService.updateChannelMessages(this.fcctService.midContent.docID, this.fcctService.midContent.messages);
+      } else {
+        this.chatService.updateChatMessages(this.fcctService.midContent.docID, this.fcctService.midContent.messages);
+      }
+      this.clearInput();
+    }
+
+  }
+
+  deleteImg(i) {
+    this.messageImages.splice(i,1);
+  }
 }
